989e625a04b73a0994d572cc9aef789ec0e7ee55
Marisa Ramon <mer0522@gmail.com>
2019-07-10 20:33:11 -0500

    user can see list of products in database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,4 +12,37 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
+    readProducts();
+    start();
 });
+
+function readProducts() {
+    console.log("Selecting all products.....\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+}
+
+// function which prompts the user for what action they should take
+function start() {
+    inquirer
+      .prompt({
+        name: "postOrBid",
+        type: "list",
+        message: "Would you like to [POST] an auction or [BID] on an auction?",
+        choices: ["POST", "BID", "EXIT"]
+      })
+      .then(function(answer) {
+        // based on their answer, either call the bid or the post functions
+        if (answer.postOrBid === "POST") {
+          postAuction();
+        }
+        else if(answer.postOrBid === "BID") {
+          bidAuction();
+        } else{
+          connection.end();
+        }
+      });
+  }
