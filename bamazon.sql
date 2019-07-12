0ae3f0e182cd6b2d5bcfe51f62ceccb1a8902c81
Marisa Ramon <mer0522@gmail.com>
2019-07-12 14:01:41 -0500

    add some js functionality

--- bamazon.sql
+++ bamazon.sql
@@ -12,5 +12,9 @@ CREATE TABLE products(
     PRIMARY KEY (item_id)
 );
 
+
+
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("trampoline", "sporting goods", 50.00, 1), ("pajamas", "clothing", 19.99, 5), ("candle", "decor", 5.00, 3), ("table lamp", "decor", 50.00, 2), ("red dress", "clothing", 100, 2), ("pool float", "sporting goods", 30, 2), ("art work", "decor", 159, 4), ("sofa", "furniture", 300, 2), ("bed", "furniture", 500, 1);
+
+select * from products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("console.table");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -10,40 +11,28 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
+    if (err) {console.log("err: " +err)}
     console.log("connected as id " + connection.threadId + "\n");
     readProducts();
-    buy();
+    //buy();
 });
 
 function readProducts() {
     console.log("Selecting all products.....\n");
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-        console.log(res);
-        connection.end();
+        console.table(res);
+        promptForId(res);
     });
 }
 
-function buy() {
+function promptForId(inventory) {
     inquirer
       .prompt([
         {
           name: "item_id",
           type: "input",
           message: "What is the id of the product you would like to buy?",
-          //loop through and display product id's again?
-          validate: function(value) {
-            if (isNaN(value) === false) {
-              return true;
-            }
-            return false;
-          }
-        },
-        {
-          name: "units",
-          type: "input",
-          message: "How many units would you like to buy?",
           validate: function(value) {
             if (isNaN(value) === false) {
               return true;
@@ -51,25 +40,79 @@ function buy() {
             return false;
           }
         }
+        
       ])
       .then(function(answer) {
         // when finished prompting, check if store has enough product to meet customer's request
         // if quantity of item with that id < store's quantity, can proceed with transaction
             // show customer total cost of purchase and update store's inventory
         // else, insufficient quantity and prevent order from going through
-        connection.query(
-          "INSERT INTO auctions SET ?",
-          {
-            item_name: answer.item,
-            category: answer.category,
-            starting_bid: answer.startingBid || 0,
-            highest_bid: answer.startingBid || 0
-          },
-          function(err) {
-            if (err) throw err;
-            console.log("Your auction was created successfully!");
+    //     connection.query(
+    //       "INSERT INTO auctions SET ?",
+    //       {
+    //         item_name: answer.item,
+    //         category: answer.category,
+    //         starting_bid: answer.startingBid || 0,
+    //         highest_bid: answer.startingBid || 0
+    //       },
+    //       function(err) {
+    //         if (err) throw err;
+    //         console.log("Your auction was created successfully!");
         
+    //       }
+    //     );
+    var id = parseInt(answer.item_id);
+    var product = checkInventory(id, inventory);
+    if(product){
+        promptForQuantity(product);
+    }
+    else{
+        console.log("the item id is invalid");
+        readProducts();
+
+    }
+
+    });
+  }
+  function checkInventory(id, inventory){
+      for(var i=0; i< inventory.length; i++){
+          if(inventory[i].item_id === id){
+              return inventory[i];
           }
-        );
-      });
+      }
+      return null;
+
+  }
+
+  function promptForQuantity(product){
+    inquirer
+    .prompt([
+      {
+        name: "units",
+        type: "input",
+        message: "How many units would you like to buy?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ])
+    .then(function(answer) {
+        var quantity = parseInt(answer.units);
+        if(quantity > product.stock_quantity){
+            console.log("insuffient quantity");
+            readProducts()
+        }
+        else{
+            makePurchase(product, quantity);
+        }
+    });
+  }
+
+  function makePurchase(product, quantity){
+      console.log("make purchase");
+
+
   }
